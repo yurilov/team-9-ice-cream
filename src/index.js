1f137b116b995ea66a3277836517136eae955027
Yurii Shchepilov <yuraschepilov@gmail.com>
2021-11-14 18:10:56 +0200

    добавлено кнопку back-to-top

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
-import "./js/modal";
-import "./js/franchise-modal";
-import "./js/buy-modal";
+import "./js/modal.js";
+import "./js/franchise-modal.js";
+import "./js/buy-modal.js";
 import './js/flip.js';
+import './js/back-to-top.js'
