553d2472b03298277d38d0941fe207a4c1d74c8a
Yurii Shchepilov <yuraschepilov@gmail.com>
2021-11-14 20:17:38 +0200

    базовые стили мобильного меню

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import "./js/modal.js";
 import "./js/franchise-modal.js";
 import "./js/buy-modal.js";
 import './js/flip.js';
-import './js/back-to-top.js'
+import './js/back-to-top.js';
+import './js/menu.js';
