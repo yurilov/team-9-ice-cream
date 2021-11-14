880a05f28caeda869c9cb026e5ef07010013c628
Konurbaeva <konurbaevamadina@gmail.com>
2021-11-14 16:48:47 +0100

    added slider and styles

---
+++ src/js/slick.js
@@ -0,0 +1,10 @@
+
+$('.slider').slick({
+     infinite: true,
+      dots: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      fade: true,
+      fadeSpeed: 1000
+  });
