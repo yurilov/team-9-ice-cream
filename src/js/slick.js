75dcb99989ea6f249a5e53dd4fede196d2495679
Konurbaeva <konurbaevamadina@gmail.com>
2021-11-14 22:10:54 +0100

    center an img

--- src/js/slick.js
+++ src/js/slick.js
@@ -2,9 +2,5 @@
 $('.slider').slick({
      infinite: true,
       dots: true,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      fade: true,
-      fadeSpeed: 1000
+      arrows: false
   });
