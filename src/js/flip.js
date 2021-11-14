c619014b3eeeb7fe8be70efad846c24847b9bdf4
Yurii Shchepilov <yuraschepilov@gmail.com>
2021-11-14 14:29:50 +0200

    поправлен скрипт

--- src/js/flip.js
+++ src/js/flip.js
@@ -1,9 +1,43 @@
 (() => {
   const refs = {
-    rotateCardBtn: document.querySelector('[data-rotate-front]'),
-    unrotateCardBtn: document.querySelector('[data-rotate-back]'),
-      cardFront: document.querySelector('[data-card-front]'),
-    cardBack: document.querySelector('[data-card-back]'),
+    rotateCardBtn: document.querySelector('[data-cream-rotate-front]'),
+    unrotateCardBtn: document.querySelector('[data-cream-rotate-back]'),
+    cardFront: document.querySelector('[data-cream-front]'),
+    cardBack: document.querySelector('[data-cream-back]'),
+  };
+
+  refs.rotateCardBtn.addEventListener('click', toggleCard);
+  refs.unrotateCardBtn.addEventListener('click', toggleCard);
+
+  function toggleCard() {
+      refs.cardFront.classList.toggle('flipped');
+      refs.cardBack.classList.toggle('flipped');
+  }
+})();
+
+(() => {
+  const refs = {
+    rotateCardBtn: document.querySelector('[data-coffee-rotate-front]'),
+    unrotateCardBtn: document.querySelector('[data-coffee-rotate-back]'),
+    cardFront: document.querySelector('[data-coffee-front]'),
+    cardBack: document.querySelector('[data-coffee-back]'),
+  };
+
+  refs.rotateCardBtn.addEventListener('click', toggleCard);
+  refs.unrotateCardBtn.addEventListener('click', toggleCard);
+
+  function toggleCard() {
+      refs.cardFront.classList.toggle('flipped');
+      refs.cardBack.classList.toggle('flipped');
+  }
+})();
+
+(() => {
+  const refs = {
+    rotateCardBtn: document.querySelector('[data-milkshakes-rotate-front]'),
+    unrotateCardBtn: document.querySelector('[data-milkshakes-rotate-back]'),
+    cardFront: document.querySelector('[data-milkshakes-front]'),
+    cardBack: document.querySelector('[data-milkshakes-back]'),
   };
 
   refs.rotateCardBtn.addEventListener('click', toggleCard);
