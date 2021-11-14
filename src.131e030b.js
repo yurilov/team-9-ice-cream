434dceb9e0a25b0cd01f3b153b4a21a9ed892f3b
yurilov <89077590+yurilov@users.noreply.github.com>
2021-11-14 13:26:16 +0000

    Deploying to gh-pages from @ yurilov/team-9-ice-cream@c328fc68cef1fa2e795d09114ee836e7457aa769 🚀

--- src.131e030b.js
+++ src.131e030b.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/flip.js");
 },{"./sass/main.scss":"clu1","./js/flip.js":"z9QZ"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-9-ice-cream/src.f566dd6a.js.map
+//# sourceMappingURL=/team-9-ice-cream/src.131e030b.js.map
